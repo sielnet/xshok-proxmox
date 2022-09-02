c7fc9ab626881e865d425bd05abf4faf06d9b928
sielnet <89866652+sielnet@users.noreply.github.com>
2022-09-02 17:41:15 +0200

    Update install-post.sh

--- install-post.sh
+++ install-post.sh
@@ -351,26 +351,26 @@ fi
 if [ "${XS_ZFSAUTOSNAPSHOT,,}" == "yes" ] ; then
     ## Install zfs-auto-snapshot
     /usr/bin/env DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::='--force-confdef' install zfs-auto-snapshot
-    # make 5min snapshots , keep 12 5min snapshots
+    # make 15min snapshots , keep 4 5min snapshots
     if [ -f "/etc/cron.d/zfs-auto-snapshot" ] ; then
-      sed -i 's|--keep=[0-9]*|--keep=12|g' /etc/cron.d/zfs-auto-snapshot
-      sed -i 's|*/[0-9]*|*/5|g' /etc/cron.d/zfs-auto-snapshot
+      sed -i 's|--keep=[0-9]*|--keep=4|g' /etc/cron.d/zfs-auto-snapshot
+      sed -i 's|*/[0-9]*|*/15|g' /etc/cron.d/zfs-auto-snapshot
     fi
-    # keep 24 hourly snapshots
+    # keep 1 hourly snapshots
     if [ -f "/etc/cron.hourly/zfs-auto-snapshot" ] ; then
-      sed -i 's|--keep=[0-9]*|--keep=24|g' /etc/cron.hourly/zfs-auto-snapshot
+      sed -i 's|--keep=[0-9]*|--keep=1|g' /etc/cron.hourly/zfs-auto-snapshot
     fi
-    # keep 7 daily snapshots
+    # keep 1 daily snapshots
     if [ -f "/etc/cron.daily/zfs-auto-snapshot" ] ; then
-      sed -i 's|--keep=[0-9]*|--keep=7|g' /etc/cron.daily/zfs-auto-snapshot
+      sed -i 's|--keep=[0-9]*|--keep=1|g' /etc/cron.daily/zfs-auto-snapshot
     fi
-    # keep 4 weekly snapshots
+    # keep 1 weekly snapshots
     if [ -f "/etc/cron.weekly/zfs-auto-snapshot" ] ; then
-      sed -i 's|--keep=[0-9]*|--keep=4|g' /etc/cron.weekly/zfs-auto-snapshot
+      sed -i 's|--keep=[0-9]*|--keep=1|g' /etc/cron.weekly/zfs-auto-snapshot
     fi
-    # keep 3 monthly snapshots
+    # keep 1 monthly snapshots
     if [ -f "/etc/cron.monthly/zfs-auto-snapshot" ] ; then
-      sed -i 's|--keep=[0-9]*|--keep=3|g' /etc/cron.monthly/zfs-auto-snapshot
+      sed -i 's|--keep=[0-9]*|--keep=1|g' /etc/cron.monthly/zfs-auto-snapshot
     fi
 fi
 
